a0f8b54cbdec4d27f02e874dacf87bbbe4dea4a8
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-03-16 14:02:45 +0100

    foto poster

---
+++ src/components/CardItem.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        
+        <figure>
+            <img :src="poster + element.poster_path" alt="">
+        </figure>
+        <div>
+            <h3>{{ element.title }}</h3>
+            <h3 v-show="(element.title === element.original_title) ? false : true ">
+                {{ element.original_title }}
+            </h3>
+        </div>
+        <div>
+            <p>{{ element.original_language }}</p>
+            <p>{{ element.vote_average }}</p>
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'cardItem',
+    props: {
+        element: {
+            type: Object,
+            required: true,
+        }
+    },
+    data() {
+        return {
+            poster: 'https://image.tmdb.org/t/p/w342',
+            img: {
+                ita: require('@/assets/img/ita.png'),
+                eng: require('@/assets/img/eng.png'),
+            },
+        };
+    },
+
+    methods: {
+
+        setLanguage: function(language) {
+
+            if (language === 'it') {
+                return this.img.ita;
+            } else if (language === 'en') {
+                return this.img.eng;
+            }
+        },
+    },
+}
+
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,7 +1,7 @@
 <template>
     <main id="film">
 
-        <ol v-for="movie in movies" :key="movie.id">
+        <!-- <ol v-for="movie in movies" :key="movie.id">
             <li>Titolo: {{ movie.title }}</li>
             <li v-show="(movie.title === movie.original_title) ? false : true ">
                 Titolo Originale: {{ movie.original_title }}
@@ -10,7 +10,9 @@
                 <img :src="setLanguage(movie.original_language)" :alt="movie.original_language">
             </li>
             <li>Rating: {{ movie.vote_average }}</li>
-        </ol>
+        </ol> -->
+
+        <cardItem v-for="movie in movies" :key="movie.id" :element="movie"/>
 
     </main>
 </template>
@@ -18,29 +20,12 @@
 <script>
 
 import state from '../store.js';
+import cardItem from './CardItem.vue';
 
 export default {
     name: 'mainContent',
-
-    data() {
-        return {
-            img: {
-                ita: require('@/assets/img/ita.png'),
-                eng: require('@/assets/img/eng.png'),
-            },
-        };
-    },
-
-    methods: {
-
-        setLanguage: function(language) {
-
-            if (language === 'it') {
-                return this.img.ita;
-            } else if (language === 'en') {
-                return this.img.eng;
-            }
-        },
+    components: {
+        cardItem,
     },
 
     computed: {
@@ -50,22 +35,13 @@ export default {
         }
     }
 }
+
 </script>
 
 <style lang="scss" scoped>
 
 #film {
     padding: 20px 10px;
-
-    .languages {
-        width: 25px;
-        height: 25px;
-    
-        img {
-            object-fit: cover;
-            object-position: center;
-        }
-    }
 }
 
 </style>
